640ed9c8dac14447d95d3eca2155f1485ce08bd8
ryandefea <ryan.defea@gmail.com>
2022-08-12 14:15:22 -0500

    update databse name.

--- Pierre/Models/Treat.cs
+++ Pierre/Models/Treat.cs
@@ -10,11 +10,7 @@ namespace Pierre.Models
             this.JoinEntities = new HashSet<FlavorTreat>();
         }
         public int TreatId { get; set; }
-        // public int Rating { get; set; }
         public string Name { get; set; }
-        // public string Instruction { get; set; }
-
-        // public int FlavorId { get; set; }
         public virtual Flavor Flavor { get; set; }
 
         public virtual ApplicationUser User { get; set; }
