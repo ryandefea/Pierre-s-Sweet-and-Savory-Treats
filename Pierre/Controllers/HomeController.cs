36dca4b37881e6e8d29d201b3974ad4a5d83ee2e
ryandefea <ryan.defea@gmail.com>
2022-08-12 11:07:55 -0500

    update home and treats controllers.

--- Pierre/Controllers/HomeController.cs
+++ Pierre/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace RecipeBox.Controllers
+namespace Pierre.Controllers
 {
   public class HomeController : Controller
   {
--- Pierre/Controllers/TreatsController.cs
+++ Pierre/Controllers/TreatsController.cs
@@ -1,22 +1,22 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using RecipeBox.Models;
+using Pierre.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
-namespace RecipeBox.Controllers
+namespace Pierre.Controllers
 {
   [Authorize]
-  public class RecipesController : Controller
+  public class TreatsController : Controller
   {
-    private readonly RecipeBoxContext _db;
+    private readonly PierreContext _db;
     private readonly UserManager<ApplicationUser> _userManager; 
 
-    public RecipesController(UserManager<ApplicationUser> userManager, RecipeBoxContext db)
+    public TreatsController(UserManager<ApplicationUser> userManager, PierreContext db)
     {
       _userManager = userManager;
       _db = db;
@@ -26,27 +26,27 @@ namespace RecipeBox.Controllers
     {
         var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         var currentUser = await _userManager.FindByIdAsync(userId);
-        var userRecipes = _db.Recipes.Where(entry => entry.User.Id == currentUser.Id).ToList();
-        return View(userRecipes);
+        var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+        return View(userTreats);
     }
 
     public ActionResult Create()
     {
-      ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View();
     }
 
     [HttpPost]
-    public async Task<ActionResult> Create(Recipe recipe, int TagId, string Name)
+    public async Task<ActionResult> Create(Treat treat, int FlavorId, string Name)
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
-      recipe.User = currentUser;
-      _db.Recipes.Add(recipe);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
       _db.SaveChanges();
-      if (TagId != 0)
+      if (FlavorId != 0)
       {
-        _db.TagRecipe.Add(new TagRecipe() { TagId = TagId, RecipeId = recipe.RecipeId });
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -54,70 +54,70 @@ namespace RecipeBox.Controllers
 
     public ActionResult Details(int id)
     {
-      var thisRecipe = _db.Recipes
-          .Include(recipe => recipe.JoinEntities)
-          .ThenInclude(join => join.Tag)
-          .FirstOrDefault(recipe => recipe.RecipeId == id);
-      return View(thisRecipe);
+      var thisTreat = _db.Treats
+          .Include(treat => treat.JoinEntities)
+          .ThenInclude(join => join.Flavor)
+          .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
     }
     
     public ActionResult Edit(int id)
     {
-      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
-      ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Name");
-      return View(thisRecipe);
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
     }
 
     [HttpPost]
-    public ActionResult Edit(Recipe recipe, int TagId)
+    public ActionResult Edit(Treat treat, int FlavorId)
     {
-      if (TagId != 0)
+      if (FlavorId != 0)
       {
-        _db.TagRecipe.Add(new TagRecipe() { TagId = TagId, RecipeId = recipe.RecipeId });
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
-      _db.Entry(recipe).State = EntityState.Modified;
+      _db.Entry(treat).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Delete(int id)
     {
-      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
-      return View(thisRecipe);
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
-      _db.Recipes.Remove(thisRecipe);
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
-    public ActionResult AddTag(int id)
+    public ActionResult AddFlavor(int id)
     {
-      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
-      ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Name");
-      return View(thisRecipe);
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
     }
 
     [HttpPost]
-    public ActionResult AddTag(Recipe recipe, int TagId)
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
     {
-      if (TagId !=0)
+      if (FlavorId !=0)
       {
-        _db.TagRecipe.Add(new TagRecipe() { TagId = TagId, RecipeId = recipe.RecipeId }); 
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId }); 
         _db.SaveChanges();
       }
       return RedirectToAction("Index");
     }
 
     [HttpPost]
-    public ActionResult DeleteTag(int joinId)
+    public ActionResult DeleteFlavor(int joinId)
     {
-      var joinEntry = _db.TagRecipe.FirstOrDefault(entry => entry.TagRecipeId == joinId);
-      _db.TagRecipe.Remove(joinEntry);
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -130,7 +130,7 @@ namespace RecipeBox.Controllers
     [HttpPost]
     public ActionResult ShowSearch(string searchPhrase)
     {
-        List<Recipe> model = _db.Recipes.Where(p => p.Name.ToLower().Contains(searchPhrase.ToLower()) || p.Tag.Name.ToLower().Contains(searchPhrase.ToLower())).ToList(); 
+        List<Treat> model = _db.Treats.Where(p => p.Name.ToLower().Contains(searchPhrase.ToLower()) || p.Flavor.Name.ToLower().Contains(searchPhrase.ToLower())).ToList(); 
         return View("Index", model);
     }
   }
