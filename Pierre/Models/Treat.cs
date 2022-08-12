e09e1533606a31236956bc5dc85aea9a9ccd699b
ryandefea <ryan.defea@gmail.com>
2022-08-12 13:36:59 -0500

    update views.

--- Pierre/Models/Treat.cs
+++ Pierre/Models/Treat.cs
@@ -10,9 +10,9 @@ namespace Pierre.Models
             this.JoinEntities = new HashSet<FlavorTreat>();
         }
         public int TreatId { get; set; }
-        public int Rating { get; set; }
+        // public int Rating { get; set; }
         public string Name { get; set; }
-        public string Instruction { get; set; }
+        // public string Instruction { get; set; }
 
         // public int FlavorId { get; set; }
         public virtual Flavor Flavor { get; set; }
