618d72baa7dad930ac4a8a2a649df0fffa9eb06b
ryandefea <ryan.defea@gmail.com>
2022-08-12 11:22:00 -0500

    update applicationuser and designtime models.

--- Pierre/Models/ApplicationUser.cs
+++ Pierre/Models/ApplicationUser.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace RecipeBox.Models
+namespace Pierre.Models
 {
     public class ApplicationUser : IdentityUser
     {
--- Pierre/Models/DesignTimeDbContext.cs
+++ Pierre/Models/DesignTimeDbContext.cs
@@ -3,23 +3,23 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
-namespace RecipeBox.Models
+namespace Pierre.Models
 {
-  public class RecipeBoxContextFactory : IDesignTimeDbContextFactory<RecipeBoxContext>
+  public class PierreContextFactory : IDesignTimeDbContextFactory<PierreContext>
   {
 
-    RecipeBoxContext IDesignTimeDbContextFactory<RecipeBoxContext>.CreateDbContext(string[] args)
+    PierreContext IDesignTimeDbContextFactory<PierreContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
 
-      var builder = new DbContextOptionsBuilder<RecipeBoxContext>();
+      var builder = new DbContextOptionsBuilder<PierreContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new RecipeBoxContext(builder.Options);
+      return new PierreContext(builder.Options);
     }
   }
 }
